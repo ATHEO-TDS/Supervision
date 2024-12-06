0401dfb721265f45c15c0b1901e9268a4001e641
ATHEO-TDS <tiago.da-silva@atheo.net>
2024-12-06 16:53:15 +0100

    return tests

--- MVM_Repositories.ps1
+++ MVM_Repositories.ps1
@@ -1,6 +1,6 @@
 # ====================================================================
 # Auteur : Tiago DA SILVA - ATHEO INGENIERIE
-# Version : 1.0.1
+# Version : 1.0.0
 # Date de création : 2024-11-29
 # Dernière mise à jour : 2024-12-02
 # Dépôt GitHub : https://github.com/ATHEO-TDS/MyVeeamMonitoring
@@ -18,17 +18,25 @@ param (
 )
 #endregion
 
-Import-Module ".\MVM_UpdateScript.psm1"
-
 #region Update Configuration
 $repoURL = "https://raw.githubusercontent.com/ATHEO-TDS/MyVeeamMonitoring/main"
 $remoteScriptURL = "$repoURL/MVM_Repositories.ps1"
 $localScriptPath = $MyInvocation.MyCommand.Path
 #endregion
 
-Update-Script -localScriptPath $localScriptPath -remoteScriptURL $remoteScriptURL
-
 #region Functions
+# Function to extract version from a script file
+function Get-ScriptVersion {
+    param (
+        [string]$ScriptContent
+    )
+    if ($ScriptContent -match "# Version\s*:\s*([\d\.]+)") {
+        return $matches[1]
+    } else {
+        return $null
+    }
+}
+
 # Functions for NRPE-style exit codes
 function Exit-OK {
     param ([string]$Message)
@@ -95,6 +103,23 @@ Function Get-VBRRepoInfo {
 }
 #endregion
 
+#region Script Update
+# Fetch local script version
+$localScriptContent = Get-Content -Path $localScriptPath -Raw
+$localVersion = Get-ScriptVersion -ScriptContent $localScriptContent
+
+# Fetch remote script version
+$remoteScriptContent = Invoke-RestMethod -Uri $remoteScriptURL -UseBasicParsing
+$remoteVersion = Get-ScriptVersion -ScriptContent $remoteScriptContent
+
+# Update script if versions differ
+if ($localVersion -ne $remoteVersion) {
+    try {
+        $remoteScriptContent | Set-Content -Path $localScriptPath -Encoding UTF8 -Force
+    } catch {
+    }
+}
+#endregion
 
 #region Variables
 $vbrServer = "localhost"
@@ -190,6 +215,3 @@ If ($filteredRepos.count -gt 0) {
     }Else{
         Exit-Unknown "No replica target found"
     }
-
-
-
